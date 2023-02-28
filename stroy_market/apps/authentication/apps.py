beeaf0b42a2b528fdbffc8e129a827e2248842c1
berdio <berdio2908@gmail.com>
2023-02-28 13:18:21 +0500

    add auth

--- stroy_market/apps/authentication/apps.py
+++ stroy_market/apps/authentication/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.authentication'
+    verbose_name = _("Users")
+
+    def ready(self):
+        try:
+            import stroymarket.users.signals  # noqa F401
+        except ImportError:
+            pass
